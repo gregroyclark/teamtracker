3926964ecb39aea76316fdaccc5063b2bb05df70
peacefrog <gregroyclark@gmail.com>
2020-04-26 00:34:25 -0500

    creates role table (schema.sql)

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,12 @@ CREATE database trackerDB;
 USE trackerDB;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    dept_name VARCHAR(30),
+  id INT PRIMARY KEY,  
+  dept_name VARCHAR(30),  
 );
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL(10,4) NULL
+)
