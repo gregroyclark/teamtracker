0572a528f7cd8b449136b4505815ac4257b901b6
peacefrog <gregroyclark@gmail.com>
2020-04-26 01:10:10 -0500

    trims white space from mysql connection block, leaves a comment for next working step

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "trackerDB"
+    //put this info into .env, replace info with process.env
+  host: "localhost",  
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "trackerDB"  
   });
 
   connection.connect(function(err) {
