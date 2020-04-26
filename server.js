cfb7ff12e2a63a0b6b9067f2e41b9c4f920ff71c
peacefrog <gregroyclark@gmail.com>
2020-04-26 01:04:28 -0500

    trims comments from mysql connection block

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "",
     database: "trackerDB"
   });
