d00ce2030e64d6210aa35a867dde899a044e3697
peacefrog <gregroyclark@gmail.com>
2020-07-25 20:51:52 -0500

    comments promise, starts async function

--- app.js
+++ app.js
@@ -27,18 +27,21 @@ function promptUser() {
     }
   ]);
 }
+
+async function init() {
   
-  .then(function(data) {
+}
+  
+  // .then(function(data) {
 
-    var employee = data.name.toLowerCase().split(' ').join('') + ".json";
+  //   var employee = data.name.toLowerCase().split(' ').join('') + ".json";
 
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+  // fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
 
-    if (err) {
-      return console.log(err);
-    }
+  //   if (err) {
+  //     return console.log(err);
+  //   }
 
-    console.log("Success!");
+  //   console.log("Success!");
 
-  });
-});
+  // });
